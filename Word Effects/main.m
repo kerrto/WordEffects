ed8eed69177212eadf7ccb3e2f59baf67eed4978
Kerry Toonen <kerrytoonen@gmail.com>
2016-01-11 17:41:03 -0500

    Initial Commit

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by Kerry Toonen on 2016-01-11.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
